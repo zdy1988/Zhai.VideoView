75b619bccb0971bea1c9a498d9c02511f5f52a20
张大爷 <virus_zhh@126.com>
2023-02-19 23:30:23 +0800

    11

--- src/XamlServices/VideoThumbService.cs
+++ src/XamlServices/VideoThumbService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -75,6 +76,75 @@ namespace Zhai.VideoView.XamlServices
                     taskQueue.Run(() => source.DrawThumb());
                 }
             }
+        }
+
+
+
+        public static readonly DependencyProperty VideoDirectoryProperty = DependencyProperty.RegisterAttached("VideoDirectory", typeof(DirectoryInfo), typeof(VideoThumbService), new PropertyMetadata(new PropertyChangedCallback(OnVideoDirectoryPropertyChangedCallback)));
+        public static DirectoryInfo GetVideoDirectory(DependencyObject obj) => (DirectoryInfo)obj.GetValue(VideoDirectoryProperty);
+        public static void SetVideoDirectory(DependencyObject obj, object value) => obj.SetValue(VideoDirectoryProperty, value);
+        private static void OnVideoDirectoryPropertyChangedCallback(DependencyObject sender, DependencyPropertyChangedEventArgs args)
+        {
+            if (sender is Image image)
+            {
+                if (image.IsLoaded)
+                {
+                    return;
+                }
+
+                var directory = GetVideoDirectory(image);
+
+                if (directory == null)
+                {
+                    image.Source = VideoThumbStateResources.ImageFailed;
+                    return;
+                }
+
+                var filename = FindCover(directory.FullName, directory.Name);
+
+                if (string.IsNullOrEmpty(filename))
+                {
+                    var file = directory.EnumerateFiles().Where(VideoSupport.VideoSupportExpression).FirstOrDefault();
+
+                    if (file != null)
+                        filename = file.FullName;
+                }
+
+                if (!string.IsNullOrEmpty(filename))
+                {
+                    var source = new VideoThumbBase(filename);
+
+                    var binding = new Binding()
+                    {
+                        Source = source,
+                        Path = new PropertyPath("ThumbSource")
+                    };
+
+                    image.SetBinding(Image.SourceProperty, binding);
+
+                    source.DrawThumb();
+                }
+            }
+        }
+
+        private static string FindCover(string directory, string name)
+        {
+            string[] reservePaths = new string[] { $"{directory}\\cover", $"{directory}\\{name}" };
+
+            foreach (var path in reservePaths)
+            {
+                foreach (var format in new string[] { "jpg", "jpeg", "png" })
+                {
+                    string cover = $"{path}.{format}";
+
+                    if (File.Exists(cover))
+                    {
+                        return cover;
+                    }
+                }
+            }
+
+            return string.Empty;
         }
     }
 }
